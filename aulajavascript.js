6bcd8e12e2d806b8c2f1518a1293644417933c4b
WellingtonEspinarde <wellingtonespinarde@gmail.com>
2024-11-07 15:35:27 -0300

    Update aulajavascript.js

--- aulajavascript.js
+++ aulajavascript.js
@@ -3,3 +3,9 @@ function Ligar(){
     document.getElementById('lamp').src = 
     'https://www.w3schools.com/js/pic_bulbon.gif'
 }
+
+function Desligar(){
+    document.getElementById('lamp').src = 
+    'https://www.w3schools.com/js/pic_bulboff.gif'
+}
+
